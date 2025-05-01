e6c862feadd220be1ec7e02eca803fa63fa83b24
ramytin <ramtin99akbari@gmail.com>
2025-05-01 12:04:04 +0100

    Add files via upload

---
+++ One-Page Website/JS/one_page_website.js
@@ -0,0 +1,34 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const captionText = document.getElementById("caption");
+
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  captionText.innerHTML = slides[slideIndex - 1].querySelector("img").alt;
+}
